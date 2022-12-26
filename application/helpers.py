14e97d947e4b86cf87f4b3be39323d39244f5071
Sahil Rajpal <sahilrajpal121@gmail.com>
2022-12-26 22:36:53 +0530

    updated the project with few app routes

---
+++ application/helpers.py
@@ -0,0 +1,16 @@
+from email_validator import validate_email, EmailNotValidError
+from flask import flash
+import re
+
+def wrong_email_input(email):
+    try:
+        validate_email(email)
+    except EmailNotValidError as e:
+        flash(e)
+        return True
+    
+def valid_username(username):
+    username_valid = re.match('^[a-zA-Z_\d\.]+$', username)
+    if username_valid is not None:
+        return True
+    return False
